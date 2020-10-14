d1551821b0dfa1507e39abff4d0affdfff68cf7b
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-10-14 16:07:42 +0200

    updated upload of picutres

--- uploadPicture.php
+++ uploadPicture.php
@@ -1,53 +1,72 @@
 <?php
 
-
 require_once './sql_queries.inc';
+require_once 'cors.inc';
+
+cors();
 
 
-$allowedFileTypes = ['png', 'jpg', 'jpeg', 'gif'];
+function fileTypeAllowed(string $fileType): bool {
+  return in_array($fileType, ['png', 'jpg', 'jpeg', 'gif']);
+}
+
+/**
+ * @param string $manuscriptId
+ * @return string
+ * @throws Exception
+ */
+function doUpload(string $manuscriptId): string {
+  // check if manuscript exists
+  if (!manuscriptMetaDataById($manuscriptId)) {
+    throw new Exception('No such manifest exists!');
+  }
+
+  if (count($_FILES) == 0) {
+    throw new Exception('No files were uploaded!');
+  }
+
+  $file = $_FILES['file'];
+
+  // check if uploaded file is image
+  if (getimagesize($file['tmp_name']) === false) {
+    throw new Exception('File is no image!');
+  }
+
+  $targetFileName = basename($file['name']);
+
+  $targetDir = "./uploads/$manuscriptId/";
+
+  $targetFile = $targetDir . $targetFileName;
+
+// check
+  $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+  if (!fileTypeAllowed($imageFileType)) {
+    throw new Exception("File type $imageFileType is not allowed!");
+  }
+
+  if (file_exists($targetFile)) {
+    throw new Exception('File already exists!');
+  }
+
+  if (!file_exists($targetDir)) {
+    mkdir($targetDir);
+  }
+
+  $fileSaved = move_uploaded_file($file['tmp_name'], $targetFile);
+  if (!$fileSaved) {
+    throw new Exception('File could not be saved');
+  }
+
+  return $targetFileName;
+}
+
 
 $manuscriptId = $_GET['id'];
 
-// check if manuscript exists
-if (!manuscriptMetaDataById($manuscriptId)) {
-  exit('No such manifest exists!');
+try {
+  $targetFileName = doUpload($manuscriptId);
+
+  echo(json_encode(["fileName" => $targetFileName]));
+} catch (Exception $e) {
+  echo(json_encode(["error" => $e->getMessage()]));
 }
-
-$targetDir = "./uploads/$manuscriptId/";
-
-if (count($_FILES) == 0) {
-  exit('No files!');
-}
-
-$file = $_FILES['file'];
-
-// check if uploaded file is image
-if (getimagesize($file['tmp_name']) === false) {
-  exit('File is no image!');
-}
-
-$targetFileName = basename($file['name']);
-
-$targetFile = $targetDir . $targetFileName;
-
-// check
-$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-if (!in_array($imageFileType, $allowedFileTypes)) {
-  exit("File type $imageFileType is not allowed!");
-}
-
-if (file_exists($targetFile)) {
-  exit('File already exists!');
-}
-
-if (!file_exists($targetDir)) {
-  mkdir($targetDir);
-}
-
-$fileSaved = move_uploaded_file($file['tmp_name'], $targetFile);
-if (!$fileSaved) {
-  exit('File could not be saved');
-}
-
-echo(json_encode(["fileName" => $targetFileName]));
-
