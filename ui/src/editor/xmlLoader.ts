9d03dfa69e8671c215be5ce5f74b2dc0cf0ea54e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-07-06 11:09:16 +0200

    refactor xml loader

--- ui/src/editor/xmlLoader.ts
+++ ui/src/editor/xmlLoader.ts
@@ -1,21 +1,7 @@
 import {loadNode, XmlNode} from './xmlModel';
 
-// Document elements
-
-async function readDocumentFile(file: File): Promise<string> {
-  const fileReader = new FileReader();
-
-  return new Promise((resolve, reject) => {
-    fileReader.onloadend = () => resolve(fileReader.result as string);
-
-    fileReader.onerror = reject;
-
-    fileReader.readAsText(file);
-  });
-}
-
 export async function loadNewXml(file: File): Promise<XmlNode> {
-  const content = await readDocumentFile(file);
+  const content = await file.text();
 
   const doc = new DOMParser().parseFromString(content, 'text/xml');
 
