018312e982dc420a74430388a756ecb75206dd5c
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-11-11 13:33:38 +0100

    add font size changer for xml editor

--- ui/src/editor/EditorLeftSide.tsx
+++ ui/src/editor/EditorLeftSide.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import {NodeDisplay, NodeDisplayIProps} from './NodeDisplay';
 import {useTranslation} from 'react-i18next';
 import {EditTriggerFunc} from './editorConfig/editorConfig';
+import {IoChevronDown, IoChevronUp} from 'react-icons/io5';
 
 interface IProps extends NodeDisplayIProps {
   filename: string;
@@ -23,26 +24,39 @@ export function EditorLeftSide({
 }: IProps): JSX.Element {
 
   const {t} = useTranslation('common');
+  const [fontSize, setFontSize] = useState(1);
   const [useSerifFont, setUseSerifFont] = useState(false);
 
   return (
     <>
       <div className="box has-text-centered">{filename}<sup>&nbsp;</sup><sub>&nbsp;</sub></div>
 
-      <div className={classNames('box', 'scrollable', useSerifFont ? 'font-hpm-serif' : 'font-hpm')}>
+      <div className={classNames('box', 'scrollable', useSerifFont ? 'font-hpm-serif' : 'font-hpm')} style={{fontSize: `${fontSize}em`}}>
         <NodeDisplay node={node} currentSelectedPath={currentSelectedPath} editorConfig={editorConfig} onSelect={onNodeSelect} insertStuff={insertStuff}/>
       </div>
 
-      <div className="columns mt-2">
-        <div className="column">
+      <div className="field has-addons mt-2">
+        <div className="control">
+          <button type="button" className="button" onClick={() => setFontSize((value) => Math.round(value * 10 - 1) / 10)}><IoChevronDown/></button>
+        </div>
+        <div className="control is-expanded">
+          <input type="number" value={fontSize} step={0.1} onChange={(event) => setFontSize(() => parseFloat(event.target.value))} className="input"/>
+        </div>
+        <div className="control is-expanded">
           <button type="button" onClick={() => setUseSerifFont((use) => !use)} className="button is-fullwidth">
             {useSerifFont ? t('useSerifLessFont') : t('useSerifFont')}
           </button>
         </div>
-        <div className="column">
+        <div className="control">
+          <button type="button" className="button" onClick={() => setFontSize((value) => Math.round(value * 10 + 1) / 10)}><IoChevronUp/></button>
+        </div>
+      </div>
+
+      <div className="field has-addons">
+        <div className="control is-expanded">
           <button className="button is-fullwidth" onClick={closeFile}>{t('closeFile')}</button>
         </div>
-        <div className="column">
+        <div className="control is-expanded">
           <button type="button" onClick={exportXml} className="button is-link is-fullwidth">{t('exportXml')}</button>
         </div>
       </div>
