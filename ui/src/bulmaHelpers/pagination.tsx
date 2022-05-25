235b8b9dc7f3df7487e9f218759cad4bcee0bebe
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-05-25 14:26:26 +0200

    fix pagination

--- ui/src/bulmaHelpers/pagination.tsx
+++ ui/src/bulmaHelpers/pagination.tsx
@@ -17,11 +17,13 @@ export function MyPagination({currentPage, pageCount, previousPage, goToPage, ne
 
   return (
     <nav className="text-center">
-      <button type="button" className="px-4 py-2 rounded border border-slate-500 mr-4">{t('previousPage')}</button>
+      <button type="button" className="px-4 py-2 rounded border border-slate-500 mr-4" onClick={previousPage}>{t('previousPage')}</button>
       {Array.from({length: pageCount}, (_x, i) => i).map((i) =>
-        <button className={classNames('px-4', 'py-2', 'rounded', currentPage === i ? ['text-white', 'bg-blue-600'] : [])} key={i}>{i}</button>
+        <button className={classNames('px-4', 'py-2', 'rounded', currentPage === i ? ['text-white', 'bg-blue-600'] : [])} key={i} onClick={() => goToPage(i)}>
+          {i}
+        </button>
       )}
-      <button type="button" className="px-4 py-2 rounded border border-slate-500 ml-4">{t('nextPage')}</button>
+      <button type="button" className="px-4 py-2 rounded border border-slate-500 ml-4" onClick={nextPage}>{t('nextPage')}</button>
     </nav>
   );
 }
