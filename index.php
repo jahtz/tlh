baf5a7ff25df7306ea01ebca1302ef4b14333d9e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-02-22 10:33:38 +0100

    update index.php to display changelogs

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
 <?php
 $versions = array_filter(
   array_filter(glob('*'), 'is_dir'),
-  fn($version) => $version !== 'TLHaly' && $version !== 'stable' && $version !== 'release'
+  fn($version) => $version !== 'TLHaly' /*&& $version !== 'stable' && $version !== 'release'*/
 );
 ?>
 <!doctype html>
 <html lang="de">
   <head>
     <title>HethPort3 - TLH Beta Server</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <style>
-      .navbar {
-        margin-bottom: 30px;
-      }
-    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body>
-    <nav class="navbar is-dark"></nav>
+    <div class="container mx-auto">
+      <h1 class="my-4 font-bold text-2xl text-center">TLH Beta server</h1>
 
-    <div class="container">
-      <h1 class="title is-3 has-text-centered">TLH Beta server</h1>
+      <div class="grid grid-cols-3 gap-2">
 
-      <?php if (file_exists("./stable/index.php")): ?>
-        <div class="my-3">
-          <a href="./stable" class="button is-link is-fullwidth">Stable-Version</a>
-        </div>
-      <?php endif; ?>
+        <?php foreach ($versions as $version): ?>
+          <div class="my-3">
 
-      <?php if (file_exists("./release/index.php")): ?>
-        <div class="my-3">
-          <a href="./release" class="button is-link is-fullwidth">Release-Version</a>
-        </div>
-      <?php endif; ?>
+            <a class="p-2 block rounded-t bg-blue-500 text-white text-center" href="./<?php echo $version; ?>">
+              &hookrightarrow; <?php echo $version; ?>
+            </a>
 
-      <?php foreach ($versions as $version): ?>
-        <div class="my-3">
-          <a href="./<?php echo $version; ?>" class="button is-link is-fullwidth"><?php echo $version; ?></a>
-        </div>
-      <?php endforeach; ?>
+            <div class="p-2 border-x border-b border-slate-500">
+              <?php
+              if (file_exists("./$version.html")) {
+                include_once "./$version.html";
+              } else {
+                echo '<p class="italic text-center">No information provided.</p>';
+              }
+              ?>
+            </div>
+
+          </div>
+        <?php endforeach; ?>
+
+      </div>
 
     </div>
   </body>
