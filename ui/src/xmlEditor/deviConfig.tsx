10d43259296717a2010ecf089c03f6974cd56a73
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-06-14 13:00:00 +0200

    add missing file

---
+++ ui/src/xmlEditor/deviConfig.tsx
@@ -0,0 +1,30 @@
+import {inputClasses, XmlEditableNodeIProps, XmlEditorSingleNodeConfig} from './editorConfig';
+import {JSX} from 'react';
+import {displayReplace} from './editorConfig/displayReplacement';
+import {useTranslation} from 'react-i18next';
+import classNames from 'classnames';
+import {selectedNodeClass} from './tlhXmlEditorConfig';
+
+type deviAttrs = 'expl';
+
+export const deviConfig: XmlEditorSingleNodeConfig<'devi', deviAttrs> = {
+  replace: (node, renderChildren, isSelected/*, isLeftSide*/) => displayReplace(
+    <>(<span className={classNames('font-bold', {[selectedNodeClass]: isSelected})}>{node.attributes.expl}</span>: </>,
+    <>{renderChildren()}</>,
+    <>)</>
+  ),
+  edit: (props) => <DeviEditor {...props}/>
+};
+
+function DeviEditor({node, updateAttribute, setKeyHandlingEnabled}: XmlEditableNodeIProps<'devi', deviAttrs>): JSX.Element {
+
+  const {t} = useTranslation('common');
+
+  return (
+    <>
+      <label htmlFor="expl" className="font-bold">{t('expl')}:</label>
+      <input type="text" id="expl" className={inputClasses} defaultValue={node.attributes.expl} onFocus={() => setKeyHandlingEnabled(false)}
+             onBlur={() => setKeyHandlingEnabled(true)} onChange={(event) => updateAttribute('expl', event.target.value)}/>
+    </>
+  );
+}
