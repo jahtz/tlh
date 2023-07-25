cc3a48c1f29873559aa4f1bb54e421e66975627b
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-07-25 10:35:41 +0200

    Update layout Homepage

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -21,10 +21,6 @@ function Inner({manuscriptCount, allManuscripts, myManuscripts, page, queryPage,
 
   return (
     <>
-      <Box heading={t('newestManuscripts')}>
-        <ManuscriptsOverview manuscriptCount={manuscriptCount} allManuscripts={allManuscripts} queryPage={queryPage} page={page}/>
-      </Box>
-
       {myManuscripts && <Box heading={t('myManuscripts')}>
         <ManuscriptLinkButtons manuscripts={myManuscripts} errorMsg={t('noOwnManuscriptsYet')}/>
       </Box>}
@@ -32,6 +28,10 @@ function Inner({manuscriptCount, allManuscripts, myManuscripts, page, queryPage,
       {reviewerQueries && <ReviewerHomeBox {...reviewerQueries}/>}
 
       {executiveEditorQueries && <ExecutiveEditorHomeBox {...executiveEditorQueries}/>}
+
+      <Box heading={t('newestManuscripts')}>
+        <ManuscriptsOverview manuscriptCount={manuscriptCount} allManuscripts={allManuscripts} queryPage={queryPage} page={page}/>
+      </Box>
     </>
   );
 }
@@ -60,13 +60,16 @@ export function Home(): JSX.Element {
     <div className="container mx-auto">
       <h1 className="font-bold text-2xl text-center mb-4">TLH<sup>dig</sup></h1>
 
+      <div className="my-8 text-center">
+        <Link className="px-8 py-2 rounded bg-blue-600 text-white text-center" to={createManuscriptUrl}>
+          {t('createManuscript')}
+        </Link>
+      </div>
+
       {indexQuery.called && <WithQuery query={indexQuery}>
         {(data) => <Inner {...data} page={page} queryPage={queryPage}/>}
       </WithQuery>}
 
-      <Link className="mt-4 p-2 block rounded bg-blue-600 text-white text-center w-full" to={createManuscriptUrl}>
-        {t('createManuscript')}
-      </Link>
     </div>
   );
 }
